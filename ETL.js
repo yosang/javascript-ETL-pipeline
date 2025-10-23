5573a663241f617ff97c45bbaa5085a73a3c09ef
Yosmel Chiang <yosmelchiang@gmail.com>
2025-10-23 16:03:43 +0200

    feat: implemenet loadData functionality

--- ETL.js
+++ ETL.js
@@ -1,29 +1,52 @@
 const fs = require('fs');
-const { trimSpaces, replaceInvalid, convertToNumber } = require('./transformUtilities')
-const results = [];
+const { trimSpaces, replaceInvalid, convertToNumber } = require('./util/transformUtilities')
 
-// Converts CSV files into JSON / Objects : https://www.npmjs.com/package/csv-parser
-    // It takes a readable stream of data
-const csv = require('csv-parser') 
+// Converts CSV files into JSON / Objects: https://www.npmjs.com/package/csv-parser
+// It takes a readable stream of data
+const csv = require('csv-parser');
 
-// We can use fs's createReadStream method
-fs.createReadStream('data/employees.csv')
-    .pipe(csv())
-    .on('data', data => {
+// Converts a POJO to CSV format: 
+// We need to define the schema before using it.
 
-        const newData = {};
+function extractAndTransform () {
+    return new Promise((resolve, reject) => {
+
+        const results = [];
         
-        for(let key in data) {
-            let value = data[key];
-
-            value = trimSpaces(value);
-            value = replaceInvalid(value);  
-            value = convertToNumber(value);
-            
-            newData[key] = value;
-        }
-
-        console.log('Transformed data:', newData)
-
+        fs.createReadStream('data/employees.csv') // This method reads the file in streams of data
+        
+            // 1. Extract - Data is extracted and converted to a manageable format
+            .pipe(csv()) // Each row stream of our CSV is piped to csv-parser and converted to a POJO
+            .on('data', data => {
+                
+                const newData = {}; // Temporary object that serves as a row
+        
+                // 2. Transform - Data streams are transformed and immediately stored 
+                for(let key in data) {
+                    let value = data[key];
+        
+                    value = trimSpaces(value);
+                    value = replaceInvalid(value);  
+                    value = convertToNumber(value);
+                    
+                    newData[key] = value;
+                }
+                results.push(newData)
+        
+            })
+            .on('end', () => resolve(results)) // Resolve with the processed data
+            .on('error', (err) => reject(err)) // Reject the promise in case of errors
     })
-    .on('error', (error) => console.log(error))
+}
+
+async function loadData() {
+    try {
+        const processedData = await extractAndTransform();
+        // console.log(processedData) // Confirm that data is extracted and transformed as intended
+    } catch(err) {
+        console.log('Error loading data: ', err)
+    }
+}
+
+
+loadData();
